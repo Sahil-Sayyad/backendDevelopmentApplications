ba5f66371477fa16c352ea7e3feb8a20c0d2a200
Sahil Sayyad <igsahilsayyad@gmail.com>
2023-09-30 00:47:54 +0530

    sets up the user register controller and folder structeres

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
 import connectDb from "./config/mongoose";
+import routes from "./routes";
 connectDb();
 
 const app = express();
@@ -12,9 +13,11 @@ const port = process.env.PORT || 8080;
 app.use(cors({
    credentials:true,
 }));
+
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use('/', routes());
 
 const server = http.createServer(app);
 server.listen(port, ()=>{
--- src/config/mongoose.ts
+++ src/config/mongoose.ts
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-
 const connectDb = async () => {
   try {
     const MOGNO_URL = "mongodb+srv://igsahilsayyad:9325@real-time-chat-app.q5bdt8q.mongodb.net/?retryWrites=true&w=majority";
